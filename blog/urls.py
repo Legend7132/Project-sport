9c1b3f509db63231a890ae59d4c73686c1915f94
c-daria <mercatel33@gmail.com>
2023-05-15 22:04:42 +0300

    Add Video section

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('contacts/', views.contacts, name='contacts'),
+    path('video/', views.video, name='video'),
 ]
