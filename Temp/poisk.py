db3d8b1d0c5313fa9e85e4beb03a608c14f35033
Legend7132 <artyr20032003@mail.ru>
2023-05-15 18:02:58 +0500

    Update poisk.py

--- Temp/poisk.py
+++ Temp/poisk.py
@@ -5,17 +5,13 @@ from bs4 import BeautifulSoup
 url = 'http://127.0.0.1:8000/'
 search_text = 'Hello, world!'
 
-# Получаем HTML-код страницы
 response = requests.get(url)
 html = response.text
 
-# Разбираем полученный код на отдельные элементы
 soup = BeautifulSoup(html, 'html.parser')
 
-# Ищем текст на странице
 found_text = soup.find(text=search_text)
 
-# Выводим результат
 if found_text:
     print(f'Текст "{search_text}" найден на странице')
 else:
