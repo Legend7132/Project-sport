0e273a39dd549bd5454b071b6bac10b330ad93d6
c-daria <mercatel33@gmail.com>
2023-05-28 12:31:48 +0300

    Corrected News section

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
 
 def post_list(request):
-    posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.order_by('-published_date')
+
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(posts, 10)
+
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post_list.html', {'posts': posts, 'page_obj': page_obj})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
--- news/views.py
+++ news/views.py
@@ -1,21 +1,38 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.core.paginator import Paginator
 from django.shortcuts import render
+from dateutil.parser import isoparse
+from urllib.request import Request, urlopen
+from PIL import Image
 from newsapi import NewsApiClient
 
 
 def news_list(request):
 
     newsapi = NewsApiClient(api_key ='bc16482d3ee144e2bb7933c67e386c48')
-    topnews = newsapi.get_everything(q = 'UFC -li -Fishki.net', language = 'ru')
+    topnews = newsapi.get_everything(q = 'UFC -<li>', language = 'ru')
 
     latest = topnews['articles']
     n_list = []
 
     for i in range(len(latest)):
         news = latest[i]
-        n_list.append([news['title'], news['description'], news['url'], news['urlToImage']])
 
+        n_desc = news['description']
+        if ('Fishki.net' in n_desc):
+            n_desc = n_desc.replace(n_desc[n_desc.find(' – Самые лучшие и интересные'):], '.')
+        
+        img_url = news['urlToImage']
+        img_height = 300
+        if img_url is not None:
+            req = Request(img_url, headers={'User-Agent': 'Googlebot/2.1 (+http://www.google.com/bot.html)'})
+            img = Image.open(urlopen(req))
+            img_height = img.height
+            if img.height > 300:
+                img_height = 300
+        
+        n_list.append([news['title'], n_desc, news['url'], isoparse(news['publishedAt']), img_url, img_height])
+
+    n_list.sort(key = lambda x: x[3], reverse = True)
     page_num = request.GET.get('page', 1)
     paginator = Paginator(n_list, 10)
 
